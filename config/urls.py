11dc865406b93be915e51050284969ebcdd5b81e
diomaxer <68566883+diomaxer@users.noreply.github.com>
2022-05-19 13:32:34 +0300

    second test

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import all_users, register
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', all_users, name='home'),
+    path('register', register, name='register'),
 ]
